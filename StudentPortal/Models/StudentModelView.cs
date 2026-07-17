590b021329e38aa61c01f3e6f33c1fbce9fd5fba
Krishna <krishna.s@nichebit.com>
2026-07-17 11:04:21 +0530

    Update Model Class

--- StudentPortal/Models/StudentModelView.cs
+++ StudentPortal/Models/StudentModelView.cs
@@ -1,7 +1,7 @@
 ﻿namespace StudentPortal.Models
 {
     public class StudentModelView
-    {
+    {//This is a model class
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
